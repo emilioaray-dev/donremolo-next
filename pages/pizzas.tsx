2f79388558af483b3c35483561e63cbea5202856
Celsius Aray <emilioaray@gmail.com>
2022-01-13 11:25:00 -0400

    Add Head page Pizza

--- pages/pizzas.tsx
+++ pages/pizzas.tsx
@@ -1,10 +1,16 @@
 import Header from './components/header'
 import styles from '../styles/Home.module.css'
 import CardList from './components/cardList'
+import Head from 'next/head'
 
 function Pizzas() {
   return (
     <div className={styles.backgroundHome}>
+      <Head>
+        <title>Don Rémolo | Pizzas</title>
+        <meta name="description" content="Generado por el Equipo 87 de IDeas" />
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
       <style jsx>{`
 
       `}</style>
