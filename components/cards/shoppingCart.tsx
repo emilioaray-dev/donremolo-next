0b8c40e99710b8ad7cedec3df2bf313131a62463
Celsius Aray <emilioaray@gmail.com>
2022-02-02 11:42:32 -0400

    Shoppin Cart Dev

--- components/cards/shoppingCart.tsx
+++ components/cards/shoppingCart.tsx
@@ -1,52 +1,90 @@
 import Link from "next/link";
 import Image from "next/image";
-import { FC } from "react";
+import { FC, useState } from "react";
 
 import { useRouter } from "next/router";
 
-
 const ShoppingCart: FC<{ number: number; }> = ({ number }) => {
+  let initialCount = 0;
 
-  // console.log(Lista);
+  const [count, setCount] = useState(initialCount);
+
+  let ocultar = "0";
+  if (count <= 0) {
+    ocultar = "hide";
+  }
+
+  let activar = "no";
+  if (count >= 1) {
+    activar = "activo";
+  }
+
+  console.log(activar);
   return (
     <>
       <footer>
         <div className="shopImgCart">
           <div className="shopImgCart__grid">
-            <Link href="">
-              <a href="">
-                <picture className="shopImgCart__picture">
-                  <Image className="shopImgCart__img" src="/assets/img/icons/shoppingCart.svg" alt="" width={30} height={30} />
-                </picture>
-                <div className="shoppingImgCart__badgeGrid">
-                  <span className="shopImgCart__badgeGrid--number">{number}</span>
-                </div>
-              </a>
-            </Link>
+            <button onClick={() => setCount((prevCount) => prevCount - 1)}>
+              -
+            </button>
+            <div className="ma">
+              <Link href="">
+                <a href="">
+                  <picture className={`shopImgCart__picture ${activar}`}>
+                    <Image
+                      className="shopImgCart__img"
+                      src="/assets/img/icons/shoppingCart.svg"
+                      alt=""
+                      width={30}
+                      height={28}
+                    />
+                  </picture>
+                  <div className={`shoppingImgCart__badgeGrid ${ocultar}`}>
+                    <span className="shopImgCart__badgeGrid--number">
+                      {count}
+                    </span>
+                  </div>
+                </a>
+              </Link>
+            </div>
+
+            <button onClick={() => setCount((prevCount) => prevCount + 1)}>
+              +
+            </button>
           </div>
+          <button onClick={() => setCount(initialCount)}>Reset</button>
         </div>
       </footer>
       <style jsx>{`
+        .hide {
+          display: none !important;
+        }
         .shopImgCart {
           position: fixed;
           bottom: 0;
           width: 100vw;
-          height: 4rem;
+          height: auto;
           backdrop-filter: blur(5px);
           -webkit-backdrop-filter: blur(5px);
           background: rgba(255, 255, 255, 0.5);
+          text-align: center;
         }
 
         .shopImgCart__grid {
-          display: grid;
+          display: flex;
           height: 100%;
           align-items: center;
-          justify-items: center;
+          justify-content: center;
+          flex-direction: row;
         }
 
+        .ma {
+          margin: 0px 1rem;
+        }
         .shopImgCart__picture {
           background: var(--brandColorGrey1);
-          padding: 10px 11px 10px 10px;
+          padding: 10px 11px 6px 10px;
           border-radius: 50%;
         }
 
@@ -66,9 +104,10 @@ const ShoppingCart: FC<{ number: number; }> = ({ number }) => {
           display: flex;
           justify-content: center;
           border-radius: 50%;
-          transform: translate(38px, 4px);
+          transform: translate(39px, -2px);
           color: var(--brandColorRed);
           font-size: small;
+          padding: 2px;
         }
 
         .shopImgCart__badgeGrid--number {
